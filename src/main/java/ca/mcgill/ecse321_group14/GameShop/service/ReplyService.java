6eb98f549d38814b1f6d60975bfdd6e88491dd1d
Tessa Hason <tessa.hason@mail.mcgill.ca>
2024-11-13 14:06:57 -0500

    reply and review controllers

--- src/main/java/ca/mcgill/ecse321_group14/GameShop/service/ReplyService.java
+++ src/main/java/ca/mcgill/ecse321_group14/GameShop/service/ReplyService.java
@@ -3,7 +3,9 @@ package ca.mcgill.ecse321_group14.GameShop.service;
 import ca.mcgill.ecse321_group14.GameShop.model.Manager;
 import ca.mcgill.ecse321_group14.GameShop.model.Reply;
 import ca.mcgill.ecse321_group14.GameShop.model.Review;
-import ca.mcgill.ecse321_group14.GameShop.repository.*;
+import ca.mcgill.ecse321_group14.GameShop.repository.ReviewRepository;
+import ca.mcgill.ecse321_group14.GameShop.repository.ManagerRepository;
+import ca.mcgill.ecse321_group14.GameShop.repository.ReplyRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,12 +15,32 @@ public class ReplyService {
     @Autowired
     private ReplyRepository replyRepository;
 
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Autowired 
+    private ManagerRepository managerRepository;
+
 
     @Transactional
-    public Reply createReply(String aDescription, Review aReview, Manager aManager) {
-        if (aDescription == null || aReview == null || aManager == null) {
-            throw new IllegalArgumentException("All fields must be filled!");
+    public Reply createReply(String aDescription, int reviewId, int managerId) {
+
+        if (aDescription == null){
+            throw new IllegalArgumentException("Description cannot be empty!");
         }
+
+        Review aReview = reviewRepository.findReviewById(reviewId);
+        
+        if (aReview == null){
+            throw new IllegalArgumentException("Review does not exist!");
+        }
+
+        Manager aManager = managerRepository.findManagerById(managerId);
+
+        if (aManager == null){
+            throw new IllegalArgumentException("Manager does not exist!");
+        }
+        
         Reply reply = new Reply(aDescription, aReview, aManager);
         replyRepository.save(reply);
         return reply;
