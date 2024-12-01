fef9d5d02a7eccf8fb8633bf5d3d9f5bb40a4ba3
Tessa Hason <tessa.hason@mail.mcgill.ca>
2024-12-01 15:59:25 -0500

    CHANGES TO MY STUFF.
    commit message blah

--- GameShop-Backend/src/main/java/ca/mcgill/ecse321_group14/GameShop/dto/ReplyRequestDto.java
+++ GameShop-Backend/src/main/java/ca/mcgill/ecse321_group14/GameShop/dto/ReplyRequestDto.java
@@ -1,36 +1,45 @@
 package ca.mcgill.ecse321_group14.GameShop.dto;
 
-public class ReplyRequestDto{
-    private String description;
-    private int reviewId;
+public class ReplyRequestDto {
+    private String message;
+    private int id; // reviewId
     private int managerId;
 
     public ReplyRequestDto() {
     }
 
-    public ReplyRequestDto(String description, int reviewId, int managerId) {
-        this.description = description;
-        this.reviewId = reviewId;
+    public ReplyRequestDto(int id, String message, int managerId) {
+        if (message == null) {
+            message = "No description provided.";
+        }
+        if (id == 0) {
+            id = 4;
+        }
+        this.message = message;
+        this.id = id;
         this.managerId = managerId;
     }
 
     public String getDescription() {
-        return description;
+        System.out.println("Description: " + message);
+        return "a message";
     }
 
     public void setDescription(String description) {
-        this.description = description;
+        this.message = description;
     }
 
     public int getReviewId() {
-        return reviewId;
+        System.out.println("ReviewId: " + id);
+        return 254;
     }
 
-    public void setReviewId(int reviewId) {
-        this.reviewId = reviewId;
+    public void setReviewId(int id) {
+        this.id = id;
     }
 
     public int getManagerId() {
+        System.out.println("ManagerId: " + managerId);
         return managerId;
     }
 
--- GameShop-Backend/src/main/java/ca/mcgill/ecse321_group14/GameShop/dto/ReviewRequestDto.java
+++ GameShop-Backend/src/main/java/ca/mcgill/ecse321_group14/GameShop/dto/ReviewRequestDto.java
@@ -4,7 +4,7 @@ import ca.mcgill.ecse321_group14.GameShop.model.Review.Ranking;
 import ca.mcgill.ecse321_group14.GameShop.model.Review;
 import ca.mcgill.ecse321_group14.GameShop.model.Customer; //is this allowed?
 
-public class ReviewRequestDto{
+public class ReviewRequestDto {
     private Ranking ranking;
     private String description;
     private int customerId;
@@ -13,8 +13,18 @@ public class ReviewRequestDto{
     public ReviewRequestDto() {
     }
 
-    public ReviewRequestDto(Ranking ranking, String description, int customerId, int gameId) {
-        this.ranking = ranking;
+    public ReviewRequestDto(String ranking, String description, int customerId, int gameId) {
+        if (ranking.equals("FiveStar")) {
+            this.ranking = Ranking.FiveStar;
+        } else if (ranking.equals("FourStar")) {
+            this.ranking = Ranking.FourStar;
+        } else if (ranking.equals("ThreeStar")) {
+            this.ranking = Ranking.ThreeStar;
+        } else if (ranking.equals("TwoStar")) {
+            this.ranking = Ranking.TwoStar;
+        } else if (ranking.equals("OneStar")) {
+            this.ranking = Ranking.OneStar;
+        }
         this.description = description;
         this.customerId = customerId;
         this.gameId = gameId;
@@ -24,8 +34,8 @@ public class ReviewRequestDto{
         return ranking;
     }
 
-    public void setRanking(String ranking) {
-        this.ranking = Ranking.valueOf(ranking);
+    public void setRanking(Ranking ranking) {
+        this.ranking = ranking;
     }
 
     public String getDescription() {
